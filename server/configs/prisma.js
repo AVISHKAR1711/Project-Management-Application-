bc93290ff86909e4f31e7f062269945c8128184e
AVISHKAR1711 <Avishkarp644@gmail.com>
2026-01-01 00:55:30 +0530

    Add server folder as normal folder

---
+++ server/configs/prisma.js
@@ -0,0 +1,28 @@
+import 'dotenv/config.js';
+import pkg from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
+
+const { PrismaClient } = pkg;
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaNeon({ connectionString });
+
+// Prevent multiple instances in dev (hot reload safe)
+const globalForPrisma = globalThis;
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV === 'development') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
